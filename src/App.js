bf4843943083e3c5eeea3d54b58d59698a1374bc
wei kee haw <weikeehaw@weis-MacBook-Pro.local>
2019-04-03 17:29:55 +0800

    code splitting first attempt

--- src/App.js
+++ src/App.js
@@ -1,35 +1,62 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import Page1 from "./components/page1";
-import Page2 from "./components/page2";
-import Page3 from "./components/page3";
+// import Page2 from "./components/page2";
+// import Page3 from "./components/page3";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      route: "page1"
+      route: "page1",
+      component: ""
     };
   }
 
   onRouteChange = route => {
-    this.setState({
-      route: route
-    });
+    // no code splitting
+    // this.setState({
+    //   route: route
+    // });
+    // with code splitting
+    if (route === "page1") {
+      this.setState({
+        route: route
+      });
+    } else if (route === "page2") {
+      // this is asynchronous and return promise
+      import("./components/page2").then(Page2 => {
+        this.setState({
+          route: route,
+          component: Page2.default
+        });
+      });
+    } else {
+      import("./components/page3").then(Page3 => {
+        this.setState({
+          route: route,
+          component: Page3.default
+        });
+      });
+    }
   };
 
   render() {
+    // if (this.state.route === "page1") {
+    //   return <Page1 onRouteChange={this.onRouteChange} />;
+    // } else if (this.state.route === "page2") {
+    //   return <Page2 onRouteChange={this.onRouteChange} />;
+    // } else {
+    //   return <Page3 onRouteChange={this.onRouteChange} />;
+    // }
+
     if (this.state.route === "page1") {
       return <Page1 onRouteChange={this.onRouteChange} />;
-    } else if (this.state.route === "page2") {
-      return <Page2 onRouteChange={this.onRouteChange} />;
     } else {
-      return <Page3 onRouteChange={this.onRouteChange} />;
+      return <this.state.component onRouteChange={this.onRouteChange} />;
     }
 
-    {
-      /* <header className="App-header">
+    /* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
@@ -43,7 +70,6 @@ class App extends Component {
             Learn React
           </a>
         </header> */
-    }
   }
 }
 
